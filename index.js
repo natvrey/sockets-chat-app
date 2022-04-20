0693726b3b952783e0768df937e6b7566689ae1e
Natalie Reynolds <92003973+natvrey@users.noreply.github.com>
2022-04-20 14:38:27 -0400

    Add user (dis)connected broadcast msg

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+let clients = 0;
+
+io.on("connection", function (socket) {
+  clients++;
+  io.sockets.emit("broadcast", {
+    description: clients + " clients connected!",
+  });
+  socket.on("disconnect", function () {
+    clients--;
+    io.sockets.emit("broadcast", {
+      description: clients + " clients connected!",
+    });
   });
 });
 
